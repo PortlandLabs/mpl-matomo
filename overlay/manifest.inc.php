9d291203df25a70df6b35d24edc6e19368447cd3
Korvin Szanto <korvin@portlandlabs.com>
2024-09-25 13:43:00 -0700

    Update the manifest

---
+++ overlay/manifest.inc.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * MPL merged manifest file.
+ * This file was originally built by Matomo during their release process, then was rebuilt to include changes made by
+ * MPL during release.
+ */
+
+namespace Piwik;
+class Manifest {
+    static $files = MANIFEST;
+}
